a695a598596e9eb492f8a608f01c2954813e8c00
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-08 09:57:03 -0300

    Carregando Model de produtos no Controller

--- application/controllers/Produtos.php
+++ application/controllers/Produtos.php
@@ -6,6 +6,12 @@ class Produtos extends CI_Controller
 
     public function index()
     {
+        // Carrega o Model
+        $this->load->model('produtos_model', 'produtos');
+
+        // Pega os dados do Model
+        $data['produtos'] = $this->produtos->getProdutos();
+
         $this->load->view('listarprodutos');
     }
 }
