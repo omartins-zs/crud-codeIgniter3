44f2ffbc2fc86cb3692be05feeaf902ee0a99b97
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-09 08:52:04 -0300

    Adicionando Btn para adicionar novo produto

--- application/views/listarprodutos.php
+++ application/views/listarprodutos.php
@@ -16,16 +16,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <!-- Bootstrap core CSS -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
+    <link rel="stylesheet" href="css/style.css">
+
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <style type="text/css">
-    .margin-button15 {
-        margin-bottom: 15px;
-    }
-    </style>
+
 </head>
 
 <body>
@@ -34,6 +32,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
         <div class="row">
             <h1>Lista de produtos</h1>
+
+            <a href="produtos/add" class="btn btn-success margin-button15">Novo Produto</a>
+
 
             <table class="table table-bordered">
 
