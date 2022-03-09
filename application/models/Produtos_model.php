2ef5ecda76cf9fe53c4684c4c69bf27d029705f0
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-09 10:00:25 -0300

    Criando Function para adicionar no BD

--- application/models/Produtos_model.php
+++ application/models/Produtos_model.php
@@ -8,4 +8,12 @@ class Produtos_model extends CI_Model
         $query = $this->db->get('produtos');
         return $query->result();
     }
+
+    //Adiciona um novo produtos na tabela produtos
+    public function addProduto($dados = NULL)
+    {
+        if ($dados != NULL) :
+            $this->db->insert('produtos', $dados);
+        endif;
+    }
 }
