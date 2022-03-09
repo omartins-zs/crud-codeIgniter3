0b13b759d5773fe0b78ef0e571d67987516f43df
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-09 09:51:12 -0300

    Criando Function para salvar no Banco de Dados

--- application/controllers/Produtos.php
+++ application/controllers/Produtos.php
@@ -24,4 +24,29 @@ class Produtos extends CI_Controller
         //Carrega a View
         $this->load->view('addprodutos');
     }
+
+    //Função salvar no DB
+    public function salvar()
+    {
+        //Verifica se foi passado o campo nome vazio.
+        if ($this->input->post('nome') == NULL) {
+            echo 'O compo nome do produto é obrigatório.';
+            echo '<a href="/produtos/add" title="voltar">Voltar</a>';
+        } else {
+
+            //Carrega o Model Produtos				
+            $this->load->model('produtos_model', 'produtos');
+
+            //Pega dados do post e guarda na array $dados
+            $dados['nome'] = $this->input->post('nome');
+            $dados['preco'] = $this->input->post('preco');
+            $dados['ativo'] = $this->input->post('ativo');
+
+            //Executa a função do produtos_model adicionar
+            $this->produtos->addProduto($dados);
+
+            //Fazemos um redicionamento para a página 		
+            redirect("/");
+        }
+    }
 }
