c4300a66d95dcfd087b2b1c083b971cd5d5e413f
Gabriel Martins <65038297+omartins-zs@users.noreply.github.com>
2022-06-24 12:00:03 -0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,3 +1,6 @@
+
+CREATE DATABASE produtos-hardware
+
 CREATE TABLE `produtos-hardware`.`produtos` ( `id` INT NULL AUTO_INCREMENT , `nome`  VARCHAR(100) NOT NULL , `preco` INT NOT NULL , `ativo` VARCHAR(20) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
 INSERT INTO `produtos` (`id`, `nome`, `preco`, `ativo`) VALUES (NULL, 'Placa Mae Gigabyte', '750', '1'), (NULL, 'Placa de Video Asus', '3.500', '1')
