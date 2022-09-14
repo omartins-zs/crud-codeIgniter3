d3bf3a2eeead56eb22e312cdcde498fe6aa9b947
omartins-zs <gabrielmatheus209@gmail.com>
2022-09-14 00:17:37 -0300

    Melhorando estrutura de pasta com Templates

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,22 +1,22 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-?>
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Lista de produtos da tabela produtos">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <title><?= $titulo ?></title>
 
+    <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="css/style.css">
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/cba92a6c1e.js" crossorigin="anonymous"></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css?<?= date("H:i:s"); ?>">
 
 </head>
-
-<body>
