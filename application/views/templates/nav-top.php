73dd79078ac1376d6b08384ff7880159034bf143
omartins-zs <gabrielmatheus209@gmail.com>
2022-09-18 13:42:54 -0300

    Criado Navbar em Templates

---
+++ application/views/templates/nav-top.php
@@ -0,0 +1,10 @@
+<!-- Just an image -->
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="<?= base_url() ?>assets/img/codeigniter.svg ?>" height="20" alt="MDB Logo" loading="lazy" />
+            <small>Crud CodeIgniter 3</small>
+
+        </a>
+    </div>
+</nav>
